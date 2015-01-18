aa283aeeeae993e5f3f63f2dd772dbae619930fd
Kevin Isom <kevin.isom@gmail.com>
2015-01-19 07:08:52 +1300

    organizing code better

---
+++ src/entities/turret.js
@@ -0,0 +1,45 @@
+//expected globals - game
+
+
+var Turret = function (x,y, bullets) {
+    this.base = game.add.sprite(x* game.globals.TILE_SIZE, y* game.globals.TILE_SIZE,  'turret_base');
+    this.gun = game.add.sprite((x+.5)* game.globals.TILE_SIZE, (y+.5)* game.globals.TILE_SIZE, 'turret_top');
+    this.gun.anchor.set(0.5,0.5);
+    this.bullets = bullets;
+    this.bulletTime = 0;
+}
+
+
+
+Turret.prototype.update = function() {
+    // body...
+    if(!this.target) return;
+
+
+    this.gun.rotation = game.physics.arcade.angleBetween(this.gun, this.target); 
+
+    this.fire();
+};
+
+Turret.prototype.track = function(target) {
+    this.target = target;
+};
+
+Turret.prototype.fire = function() {
+
+    if (game.time.now > this.bulletTime) { 
+        //  Grab the first bullet we can from the pool
+        var bullet = this.bullets.getFirstExists(false); 
+        if (bullet) {
+            //  And fire it
+            bullet.reset(this.gun.x+2 , this.gun.y-2 );
+            bullet.body.velocity.x = -5;
+            bullet.rotation = game.physics.arcade.moveToObject(bullet, this.target );
+            this.bulletTime = game.time.now + 800;
+            
+        }
+    }
+
+    console.log(this.target.alive);
+};
+module.exports = Turret;
