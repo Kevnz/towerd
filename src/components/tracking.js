33b43c01d8d83afcbde36ca412a97f81ed200cad
Kevin Isom <kevin.isom@gmail.com>
2015-02-07 14:43:45 +1300

    Working on tracking behavior

--- src/components/tracking.js
+++ src/components/tracking.js
@@ -1,9 +1,21 @@
-var Tracking = function () {
+module.exports = {
+    buildMove : function () {
+        var actor = this;//work in progress
+        var move = game.add.tween(actor);
+        for (var pathNode in path){
 
-}
+            // x is pf.js [path.x, path.y] or easystar.js [path.x, path.y]
+            var x = path[pathNode][0]*16 || path[pathNode].x*16,
+                y = path[pathNode][1]*16 || path[pathNode].y*16;
 
-Tracking.prototype.Move = function() {
-	// body...
+            move.to({ x: x, y: y }, 1800, Phaser.Easing.Linear.None);
+        }
+        if (actor.activeTween != null) {
+            //console.log('activeTween');
+            actor.activeTween.stop();
+         // create a new tween
+        }
+        actor.activeTween = move;
+        actor.activeTween.start(); 
+    }
 };
-
-module.exports = Tracking;
