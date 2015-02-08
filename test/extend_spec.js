4bfc1d81838e410cf95fd6060eb23ca5b9317fd0
Kevin Isom <kevin.isom@gmail.com>
2015-02-08 15:41:38 +1300

    Changed to use nodeunit

--- test/extend_spec.js
+++ test/extend_spec.js
@@ -1,22 +1,32 @@
-var test = require('tape');
- 
-test('extend test', function (t) {
-    t.plan(2);
-    
-    var extend = require('../src/utils/extend');
-    var oldObj = {
-        first:function () {
-            console.log('first');
-        },
-        prop: 'Property On Base'
-    };
-    var extendObj = {
-        second: function () {
-            console.log('second');
-        }
-    };
-    var newObj = extend(oldObj, extendObj);
 
-    t.equal(typeof newObj.second, 'function');
-    t.equal(typeof newObj.prop, 'string');
-});
+module.exports = {
+    setUp: function (callback) {
+        this.foo = 'bar';
+        callback();
+    },
+    tearDown: function (callback) {
+        // clean up
+        callback();
+    },
+    'Extend Test': function (test) {
+            var extend = require('../src/utils/extend');
+        var oldObj = {
+            first:function () {
+                console.log('first');
+            },
+            prop: 'Property On Base'
+        };
+        var extendObj = {
+            second: function () {
+                console.log('second');
+            }
+        };
+        var newObj = extend(oldObj, extendObj);
+
+        test.equal(typeof newObj.second, 'function');
+        test.equal(typeof newObj.prop, 'string'); 
+        test.done();
+    }
+};
+
+
--- test/mixin_spec.js
+++ test/mixin_spec.js
@@ -1,4 +1,3 @@
-var test = require('tape');
 var First = function () {
     console.log('First Ctor');
     return this;
@@ -9,46 +8,40 @@ First.prototype.first = function() {
 First.prototype.prop = 'Property On Base';
 
 
-test('mixin test', function (t) {
-    t.plan(2);
-    
-    var mixin = require('../src/utils/mixin');
-    var firstObj = new First();
+ 
+ 
 
-    console.log(firstObj.prototype);
-    var extendObj = {
-        second: function () {
-            console.log('second');
-        }
-    };
-    firstObj.first();
-    console.log(firstObj);
-    mixin(firstObj, extendObj);
 
-    t.equal(typeof firstObj.second, 'function');
-    t.equal(typeof firstObj.prop, 'string');
-});
-test('mixin test', function (t) {
-    t.plan(3);
-    
-    var mixin = require('../src/utils/mixin');
-    var firstObj = new First();
+module.exports = {
+    setUp: function (callback) {
+        this.foo = 'bar';
+        callback();
+    },
+    tearDown: function (callback) {
+        // clean up
+        callback();
+    },
+    'Mixin Test': function (t) {
+        var mixin = require('../src/utils/mixin');
+        var firstObj = new First();
 
-    console.log(firstObj.prototype);
-    var extendObj = {
-        second: function () {
-            console.log('second');
-        }
-    };
-    var extendOtherObj = {
-        third: function () {
-            console.log('Third');
-        }
-    };
-    firstObj.first();
-    mixin(firstObj, [extendObj,extendOtherObj]);
+        console.log(firstObj.prototype);
+        var extendObj = {
+            second: function () {
+                console.log('second');
+            }
+        };
+        var extendOtherObj = {
+            third: function () {
+                console.log('Third');
+            }
+        };
+        firstObj.first();
+        mixin(firstObj, [extendObj,extendOtherObj]);
 
-    t.equal(typeof firstObj.second, 'function');
-    t.equal(typeof firstObj.third, 'function');
-    t.equal(typeof firstObj.prop, 'string');
-});
+        t.equal(typeof firstObj.second, 'function');
+        t.equal(typeof firstObj.third, 'function');
+        t.equal(typeof firstObj.prop, 'string');
+        t.done();
+    }
+};
